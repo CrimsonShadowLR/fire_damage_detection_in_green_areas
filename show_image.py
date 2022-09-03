2838ac85e80039181caf82da278c88f7123ce488
CrimsonShadow <a20130263@pucp.pe>
2022-09-03 01:38:32 -0500

    asdafas

--- show_image.py
+++ show_image.py
@@ -57,7 +57,7 @@ def show_sample_images():
         print(basename)
         fl_split=basename.split('.')
         print(fl_split)
-        mask_path = args.masks_dir+ "/." + fl_split[0]+ "_mask."+ fl_split[1]
+        mask_path = args.masks_dir+ "/" + fl_split[0]+ "_mask."+ fl_split[1]
         y = pickle.load(open(mask_path, "rb"))
         print("Get mask for sample {}".format(idx))
 
